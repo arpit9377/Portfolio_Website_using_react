73f304334ea063f5ede25fc89f71f37420e0a2fc
Arpit Kumar Yadav <72245696+arpit9377@users.noreply.github.com>
2022-07-10 10:55:28 +0530

    Update home.js

--- src/components/Dashboard/home.js
+++ src/components/Dashboard/home.js
@@ -1,5 +1,5 @@
 import { useRef } from 'react';
-import { auth, storage, db } from '../../firebase';
+
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { addDoc } from 'firebase/firestore';
 import { collection } from 'firebase/firestore/lite';
